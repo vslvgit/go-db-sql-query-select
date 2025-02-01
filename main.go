ca29113d43cbb2232454e1937321847d057c76fb
p.v.veselov <vslv_p@mail.ru>
2025-02-01 14:14:11 +0300

    first commit

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	. "fmt"
+	"log"
 
 	_ "modernc.org/sqlite"
 )
@@ -15,27 +17,44 @@ type Sale struct {
 // String реализует метод интерфейса fmt.Stringer для Sale, возвращает строковое представление объекта Sale.
 // Теперь, если передать объект Sale в fmt.Println(), то выведется строка, которую вернёт эта функция.
 func (s Sale) String() string {
-	return fmt.Sprintf("Product: %d Volume: %d Date:%s", s.Product, s.Volume, s.Date)
+	return Sprintf("Product: %d Volume: %d Date:%s", s.Product, s.Volume, s.Date)
 }
 
 func selectSales(client int) ([]Sale, error) {
 	var sales []Sale
+	var (
+		product string
+		volume  int
+		date    int
+	)
+	sales = db.QueryRow("SELECT product, volume, date FROM sales WHERE id = :id", sql.Named("id", client))
+	err = row.Scan(&product, &volume, &date)
+	if err != nil {
+		Println(err)
+		return
 
-	// напишите код здесь
-
+	}
 	return sales, nil
 }
 
 func main() {
 	client := 208
 
+	db, err := sql.Open("sqlite", "demo.db")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer db.Close()
+
 	sales, err := selectSales(client)
 	if err != nil {
-		fmt.Println(err)
+		Println(err)
 		return
 	}
 
 	for _, sale := range sales {
-		fmt.Println(sale)
+		Println(sale)
 	}
 }
